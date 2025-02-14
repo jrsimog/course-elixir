6f288438bb700994868643814b9906137b7c487f
Jose <jose@recemed.com>
2025-02-13 21:47:08 -0300

    Trabajar con patrones y comentarios

---
+++ pratonesYTuplas.exs
@@ -0,0 +1,9 @@
+{nombre, edad} = {"Jose", 25}
+
+IO.puts("#{nombre} tiene #{edad} años")
+
+# {"Jose", 25} = {nombre, edad}, es una tupla, yh elixir permite asignar valores a variables de esta forma.
+# Los comentarios se hacen con # y se pueden hacer de una sola línea o de varias líneas con #{
+
+
+
